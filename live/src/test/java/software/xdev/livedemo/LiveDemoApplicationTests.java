925dbc284b324f01e01d099a782abb9ed9297158
JohannesRabauer <j.rabauer@xdev-software.de>
2024-02-26 07:19:54 +0100

    Commited start for live coding

---
+++ live/src/test/java/software/xdev/livedemo/LiveDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package software.xdev.livedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
