6c35cbc9873236070b24c778be18bf6f0f777143
JohannesRabauer <j.rabauer@xdev-software.de>
2024-02-26 07:51:29 +0100

    Demo finished

--- live/src/main/java/software/xdev/livedemo/LiveDemoApplication.java
+++ live/src/main/java/software/xdev/livedemo/LiveDemoApplication.java
@@ -1,13 +1,31 @@
 package software.xdev.livedemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import software.xdev.spring.data.eclipse.store.repository.config.EnableEclipseStoreRepositories;
+
 
 @SpringBootApplication
-public class LiveDemoApplication {
+@EnableEclipseStoreRepositories
+public class LiveDemoApplication implements CommandLineRunner
+{
+	private OwnerRepository ownerRepository;
+
+	public LiveDemoApplication(OwnerRepository ownerRepository)
+	{
+		this.ownerRepository = ownerRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiveDemoApplication.class, args);
 	}
 
+	@Override public void run(String... args) throws Exception
+	{
+		Owner owner = new Owner("Mick", "Fleetwood");
+		this.ownerRepository.save(owner);
+
+		this.ownerRepository.findAll().stream().forEach(System.out::println);
+	}
 }
---
+++ live/src/main/java/software/xdev/livedemo/Owner.java
@@ -0,0 +1,5 @@
+package software.xdev.livedemo;
+
+public record Owner(String firstName, String lastName)
+{
+}
